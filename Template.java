56407e865087df9fcdc18e91030c6ef89b16416e
someonw <christovanzyl0605@gmail.com>
2026-05-11 16:03:23 +0200

    better arg evaluation

--- Template.java
+++ Template.java
@@ -14,33 +14,60 @@ public class Template {
     // -m run
     String className = "";
     ArrayList<String> variables = new ArrayList<>();
+    ArrayList<String> implementations = new ArrayList<>();
+    ArrayList<String> extensions = new ArrayList<>();
     String makeRunCmd = "";
-    char processType;
+    // default is classname
+    char processType = 'c';
+    boolean evaluatedParam = true;
 
     for (int i = 0; i < args.length; i++) {
       char c = args[i].charAt(0);
       // if new process Type
       if (c == '-') {
+        if (!evaluatedParam) {
+          System.out.println("No evaluated paramaters between processes `" + args[i-1] + "` and `" + args[i] + "`.");
+          System.exit(-1);
+        }
         // arg
-        if (args[i].length() < 2) {
-          System.err.println("test");
+        char a = args[i].charAt(1);
+
+        switch (a) {
+          case 'c':
+          case 'i':
+          case 'e':
+          case 'v':
+          case 'm':
+          processType = a;
+          System.out.println("Process: `" + a + "`");
+          evaluatedParam = false;
+          break;
+          default:
+          System.out.println("Invalid argument: `" + a + "`");
           System.exit(-1);
         }
 
-        char a = args[i].charAt(1);
-        switch (c) {
-          case 'c':
-          break;
-          case 'i':
-          break;
-          case 'e':
-          break;
-          case 'v':
-          break;
-          case 'm':
-          break;
-        }
+        continue;
       }
+
+      evaluatedParam = true;
+      // if its not a new process type use the process type to evaluate the stuff
+      switch (processType) {
+        case 'c':
+        className = args[i];
+        break;
+        case 'i':
+        break;
+        case 'e':
+        break;
+        case 'v':
+        break;
+        case 'm':
+        break;
+      }
+
+      System.out.println("Process param: `" + args[i] + "`");
+
     }
   }
 
--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Test {
   public static void main(String args[]) {
     Runtime runtime = Runtime.getRuntime();
-    String cmd[] = new String[]{"java", "Template", "-c", "-"};
+    String cmd[] = new String[]{"java", "Template", "-c", "Thing", "-v"};
 
     try {
       Process process = runtime.exec(cmd);
