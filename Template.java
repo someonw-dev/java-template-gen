17dc92d97c681a8b4e94cf4cf62666f040689c14
someonw <christovanzyl0605@gmail.com>
2026-05-11 19:14:53 +0200

    mutators and accessors

--- Template.java
+++ Template.java
@@ -6,7 +6,7 @@ public class Template {
   private static String className = "";
   private static ArrayList<String> variables = new ArrayList<>();
   private static ArrayList<String> implementations = new ArrayList<>();
-  private static ArrayList<String> extensions = new ArrayList<>();
+  private static String extendsName = "";
 
   public static void main(String args[]) {
     // class name
@@ -71,7 +71,7 @@ public class Template {
         implementations.add(args[i]);
         break;
         case 'e':
-        extensions.add(args[i]);
+        extendsName = args[i];
         break;
         case 'm':
         makeRunCmd = args[i];
@@ -84,21 +84,22 @@ public class Template {
     System.out.println(className);
     System.out.println(variables);
     System.out.println(implementations);
-    System.out.println(extensions);
+    System.out.println(extendsName);
 
     createJavaOut();
     updateMake(makeRunCmd);
   }
 
   private static void createJavaOut() {
-    String classUpper = className.substring(0, 1).toUpperCase() + className.substring(1);
+    String classUpper = capitalize(className);
     try {
       FileWriter file = new FileWriter(classUpper + ".java");
       file.write("public class " + classUpper + " {\n");
 
+      writeVars(file);
       writeConstructor(file, classUpper);
-
-
+      writeAccessors(file);
+      writeMutators(file);
 
       file.write("}");
       file.close();
@@ -108,6 +109,14 @@ public class Template {
     }
   }
 
+  private static void writeVars(FileWriter file) throws IOException {
+    for (int i = 0; i<variables.size(); i += 2) {
+      file.write("\tprivate " + variables.get(i) + " "+ variables.get(i + 1) + ";\n");
+    }
+
+    file.write("\n");
+  }
+
   // default + one with vars
   private static void writeConstructor(FileWriter file, String className) throws IOException {
     writeDefCons(file, className);
@@ -115,6 +124,7 @@ public class Template {
     writeVarCons(file, className);
   }
 
+  // default constructor
   private static void writeDefCons(FileWriter file, String className) throws IOException {
     file.write("\tpublic " + className + "() {\n");
     file.write("\t\tthis(");
@@ -132,31 +142,28 @@ public class Template {
     file.write("\t}\n");
   }
 
+  // constructor with variables
   private static void writeVarCons(FileWriter file, String className) throws IOException {
+    // TODO: if you extend a class
+    // super(class type1, class type2);
+
     // constructor variables
     file.write("\tpublic " + className + "(");
+    // practically the same as above
     for (int i = 0; i<variables.size(); i += 2) {
       file.write(variables.get(i));
       file.write(" ");
       file.write(variables.get(i + 1));
-      // -3 to account for 0 indexing, the fact that the last one should be a variable name and other thing im too lazy
       if (!(i>variables.size() - 3)) {
         file.write(", ");
       }
     }
     file.write(") {\n");
 
-    file.write("\t\tthis(");
-
     // +2 since every second entry is a name and not a type
     for (int i = 0; i<variables.size(); i += 2) {
-      file.write(getDefaultFromTypePrimative(variables.get(i)));
-      // -3 to account for 0 indexing, the fact that the last one should be a variable name and other thing im too lazy
-      if (!(i>variables.size() - 3)) {
-        file.write(", ");
-      }
+      file.write("\t\t" + getMutatorFunctionWithParam(variables.get(i+1), variables.get(i+1)) + ";\n");
     }
-    file.write(");\n");
 
     file.write("\t}\n");
   }
@@ -170,23 +177,51 @@ public class Template {
       return "0";
     }
 
+    if (type.equals("char")) {
+      // since chars cant be empty
+      return "'n'";
+    }
+
     // if its not any of those im just assuming its a class
     return "new " + type + "()";
   }
 
-  private static void writeVar(FileWriter file, String type, String var) throws IOException {
-  }
-
-  private static void getAccessor(FileWriter file, String type, String var) throws IOException {
+  private static String getAccessorFunction(String var) {
+    return "get" + capitalize(var) + "()";
   }
 
   private static void writeAccessor(FileWriter file, String type, String var) throws IOException {
+    file.write("\tpublic " + type + " " + getAccessorFunction(var) + "{\n");
+    file.write("\t\treturn " + var + ";\n");
+    file.write("\t}\n");
   }
 
-  private static void getMutator(FileWriter file, String type, String var) throws IOException {
+  private static void writeAccessors(FileWriter file) throws IOException {
+    for (int i = 0; i<variables.size(); i += 2) {
+      file.write("\n");
+      writeAccessor(file, variables.get(i), variables.get(i+1));
+    }
+  }
+
+  private static String getMutatorFunctionWithParam(String var, String param) {
+    return "set" + capitalize(var) + "(" + param + ")";
+  }
+
+  private static void writeMutators(FileWriter file) throws IOException {
+    for (int i = 0; i<variables.size(); i += 2) {
+      file.write("\n");
+      writeMutator(file, variables.get(i), variables.get(i+1));
+    }
   }
 
   private static void writeMutator(FileWriter file, String type, String var) throws IOException {
+    file.write("\tpublic void " + getMutatorFunctionWithParam(var, type + " " + var) + "{\n");
+    file.write("\t\tthis." + var + " = " + var + ";\n");
+    file.write("\t}\n");
+  }
+
+  private static String capitalize(String value) {
+    return value.substring(0, 1).toUpperCase() + value.substring(1);
   }
 
   private static void updateMake(String cmdName) {
--- Thing.java
+++ Thing.java
@@ -1,8 +1,29 @@
 public class Thing {
+	private String var1;
+	private int var2;
+
 	public Thing() {
 		this("", 0);
 	}
+
 	public Thing(String var1, int var2) {
-		this("", 0);
+		setVar1(var1);
+		setVar2(var2);
 	}
+	public String getVar1(){
+		return var1;
+	}
+
+	public int getVar2(){
+		return var2;
+	}
+
+	public void setVar1(String var1){
+		this.var1 = var1;
+	}
+
+	public void setVar2(int var2){
+		this.var2 = var2;
+	}
+
 }
